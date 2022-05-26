e72502ac4be08a835fbc30e940580e3cc5f92347
simba <scottadmin@MacBook-Pro.local>
2022-05-26 21:24:26 +0600

    first commit

---
+++ src/Asd.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Asd = () => {
+    return (
+        <div>
+            <div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Asd;
